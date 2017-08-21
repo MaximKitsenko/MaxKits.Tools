5647859bfe1c22a2915737a56ab3ed33a7486cb9
Maxim Kitsenko <macros5@ya.ru>
2017-08-21 18:47:09 +0300

    patterns: prototype: tests reimplemented

--- src/Patterns/Creational/MaxKits.Patterns.Creational.Prototype.Tests/Creational/Prototype/BaseCloneableTest.cs
+++ src/Patterns/Creational/MaxKits.Patterns.Creational.Prototype.Tests/Creational/Prototype/BaseCloneableTest.cs
@@ -1,72 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using FluentAssertions;
 using MaxKits.Patterns.Tests.Creational.Prototype.Misc;
+using NUnit.Framework;
 
 namespace MaxKits.Patterns.Tests.Creational.Prototype
 {
 	internal class BaseCloneableTest
 	{
-		private Country russia;
-		private Country usa;
-		private Player player0;
-		private Player player1;
-		private Player player2;
-		private Player player3;
-		private FootballTeam footballTeamRussia;
+		private Country _russia;
+		private Country _usa;
+		private Player _player0;
+		private Player _player1;
+		private Player _player2;
+		private Player _player3;
+		private FootballTeam _footballTeamRussia;
 
+		[SetUp]
 		public void Init()
 		{
-			this.russia = new Country { Name = "Russia", Population = 145 };
-			this.usa = new Country { Name = "USA", Population = 100 };
+			this._russia = new Country { Name = "Russia", Population = 145 };
+			this._usa = new Country { Name = "USA", Population = 300 };
 
-			this.player0 = new Player() { BornCountry = this.russia, Name = "Ivanov", Number = 10 };
-			this.player1 = new Player() { BornCountry = this.russia, Name = "Petrov", Number = 9 };
-			this.player2 = new Player() { BornCountry = this.usa, Name = "Smit", Number = 23 };
-			this.player3 = new Player() { BornCountry = this.usa, Name = "Klarc", Number = 24 };
+			this._player0 = new Player() { BornCountry = this._russia, Name = "Ivanov", Number = 10 };
+			this._player1 = new Player() { BornCountry = this._russia, Name = "Petrov", Number = 9 };
+			this._player2 = new Player() { BornCountry = this._usa, Name = "Smit", Number = 23 };
+			this._player3 = new Player() { BornCountry = this._usa, Name = "Klarc", Number = 24 };
 
-			this.footballTeamRussia = new FootballTeam() { Players = new List< Player > { this.player0, this.player1 }, Captan = this.player0 };
+			this._footballTeamRussia = new FootballTeam() { Players = new List< Player > { this._player0, this._player1 }, Captan = this._player0 };
 		}
 
-		public void DeepTest()
+		[Test]
+		public void DeepCloneObject_CopyAndOriginalCompletelyUndependentObjects()
 		{
-			FootballTeam footballTeamRussiaShallow;
+			//a
+			var originalName = _footballTeamRussia.Captan.Name;
+			var originalNumber = _footballTeamRussia.Players[0].Number;
+			var footballTeamCopy = _footballTeamRussia.DeepClone();
 
-			/*
-			------------------------DEEEP-------------------------------------------------------
-			*/
-			Console.WriteLine( "-----------------------DEEP-CAPITAN-NUMBER-----------" );
+			//a
+			const int newNumber = 100500;
+			const string newName = "100500";
 
-			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			footballTeamCopy.Captan.Number = newNumber;
+			footballTeamCopy.Players[0].Name = newName;
 
-			footballTeamRussiaShallow = this.footballTeamRussia.DeepClone();
-			Console.WriteLine( "footballTeamRussiaDeep:" );
-			Console.WriteLine( footballTeamRussiaShallow.ToString() );
+			//a
+			_footballTeamRussia.Players[0].Number.Should().Be(originalNumber);
+			_footballTeamRussia.Captan.Name.Should().Be(originalName);
 
-			this.footballTeamRussia.Captan.Number = 100500;
-			Console.WriteLine( "\n\nfootballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			footballTeamCopy.Players[0].Number.Should().Be(newNumber);
+			footballTeamCopy.Captan.Name.Should().Be(newName);
 
-			Console.WriteLine( "footballTeamRussiaShallow" );
-			Console.WriteLine( footballTeamRussiaShallow.ToString() );
-			/*
-			------------------------DEEP-----------------------------------------------------
-			*/
-			Console.WriteLine( "------------------------DEEP-PLAYER-NAME-------------" );
-
-			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
-
-			footballTeamRussiaShallow = this.footballTeamRussia.DeepClone();
-			Console.WriteLine( "footballTeamRussiaShallow:" );
-			Console.WriteLine( footballTeamRussiaShallow.ToString() );
-
-			this.footballTeamRussia.Players[ 0 ].Name += "Looser";
-			Console.WriteLine( "\n\nfootballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
-
-			Console.WriteLine( "footballTeamRussiaShallow" );
-			Console.WriteLine( footballTeamRussiaShallow.ToString() );
+			object.ReferenceEquals(footballTeamCopy, _footballTeamRussia).Should().BeFalse();
 		}
 
 		public void TestShallowPlayerName()
@@ -77,16 +63,16 @@ namespace MaxKits.Patterns.Tests.Creational.Prototype
 			Console.WriteLine( "------------------------SHALLOW-PLAYER-NAME-------------" );
 
 			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
-			var footballTeamRussiaShallow = this.footballTeamRussia.ShallowClone();
+			var footballTeamRussiaShallow = this._footballTeamRussia.ShallowClone();
 			Console.WriteLine( "footballTeamRussiaShallow:" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
 
 			Console.WriteLine( "\n\nfootballTeamRussia.Players[0].Name += \"Looser\"" );
-			this.footballTeamRussia.Players[ 0 ].Name += "Looser";
+			this._footballTeamRussia.Players[ 0 ].Name += "Looser";
 			Console.WriteLine( "footballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
 			Console.WriteLine( "footballTeamRussiaShallow" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
@@ -100,16 +86,16 @@ namespace MaxKits.Patterns.Tests.Creational.Prototype
 			Console.WriteLine( "-----------------------SHALLOW-CAPITAN-NUMBER-----------" );
 
 			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
-			FootballTeam footballTeamRussiaShallow = this.footballTeamRussia.ShallowClone();
+			FootballTeam footballTeamRussiaShallow = this._footballTeamRussia.ShallowClone();
 			Console.WriteLine( "footballTeamRussiaShallow:" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
 
 			Console.WriteLine( "\n\nfootballTeamRussia.Captan.Number = 100500" );
-			this.footballTeamRussia.Captan.Number = 100500;
+			this._footballTeamRussia.Captan.Number = 100500;
 			Console.WriteLine( "footballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
 			Console.WriteLine( "footballTeamRussiaShallow" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
@@ -123,16 +109,16 @@ namespace MaxKits.Patterns.Tests.Creational.Prototype
 			Console.WriteLine( "------------------------SHALLOW-OBJECT-PLAYER-----------" );
 
 			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
-			var footballTeamRussiaShallow = this.footballTeamRussia.ShallowClone();
+			var footballTeamRussiaShallow = this._footballTeamRussia.ShallowClone();
 			Console.WriteLine( "footballTeamRussiaShallow:" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
 
 			Console.WriteLine( "\n\nfootballTeamRussia.Captan = player1" );
-			this.footballTeamRussia.Captan = this.player1;
+			this._footballTeamRussia.Captan = this._player1;
 			Console.WriteLine( "footballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
 			Console.WriteLine( "footballTeamRussiaShallow" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
@@ -146,16 +132,16 @@ namespace MaxKits.Patterns.Tests.Creational.Prototype
 			Console.WriteLine( "-----------------------SHALLOW-OBJECT-COUNTRY-----------" );
 
 			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
-			FootballTeam footballTeamRussiaShallow = this.footballTeamRussia.ShallowClone();
+			FootballTeam footballTeamRussiaShallow = this._footballTeamRussia.ShallowClone();
 			Console.WriteLine( "footballTeamRussiaShallow:" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
 
 			Console.WriteLine( "\n\nfootballTeamRussia.Captan.BornCountry = usa" );
-			this.footballTeamRussia.Captan.BornCountry = this.usa;
+			this._footballTeamRussia.Captan.BornCountry = this._usa;
 			Console.WriteLine( "footballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
 			Console.WriteLine( "footballTeamRussiaShallow" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
@@ -171,16 +157,16 @@ namespace MaxKits.Patterns.Tests.Creational.Prototype
 			Console.WriteLine( "------------------------DEEEP---PLAYER-NAME-------------" );
 
 			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
-			var footballTeamRussiaShallow = this.footballTeamRussia.DeepClone();
+			var footballTeamRussiaShallow = this._footballTeamRussia.DeepClone();
 			Console.WriteLine( "footballTeamRussiaShallow:" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
 
 			Console.WriteLine( "\n\nfootballTeamRussia.Players[0].Name += \"Looser\"" );
-			this.footballTeamRussia.Players[ 0 ].Name += "Looser";
+			this._footballTeamRussia.Players[ 0 ].Name += "Looser";
 			Console.WriteLine( "footballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
 			Console.WriteLine( "footballTeamRussiaShallow" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
@@ -194,16 +180,16 @@ namespace MaxKits.Patterns.Tests.Creational.Prototype
 			Console.WriteLine( "-----------------------DEEP----CAPITAN-NUMBER-----------" );
 
 			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
-			FootballTeam footballTeamRussiaShallow = this.footballTeamRussia.DeepClone();
+			FootballTeam footballTeamRussiaShallow = this._footballTeamRussia.DeepClone();
 			Console.WriteLine( "footballTeamRussiaShallow:" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
 
 			Console.WriteLine( "\n\nfootballTeamRussia.Captan.Number = 100500" );
-			this.footballTeamRussia.Captan.Number = 100500;
+			this._footballTeamRussia.Captan.Number = 100500;
 			Console.WriteLine( "footballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
 			Console.WriteLine( "footballTeamRussiaShallow" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
@@ -217,16 +203,16 @@ namespace MaxKits.Patterns.Tests.Creational.Prototype
 			Console.WriteLine( "------------------------DEEP----OBJECT-PLAYER-----------" );
 
 			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
-			var footballTeamRussiaShallow = this.footballTeamRussia.DeepClone();
+			var footballTeamRussiaShallow = this._footballTeamRussia.DeepClone();
 			Console.WriteLine( "footballTeamRussiaShallow:" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
 
 			Console.WriteLine( "\n\nfootballTeamRussia.Captan = player1" );
-			this.footballTeamRussia.Captan = this.player1;
+			this._footballTeamRussia.Captan = this._player1;
 			Console.WriteLine( "footballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
 			Console.WriteLine( "footballTeamRussiaShallow" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
@@ -240,16 +226,16 @@ namespace MaxKits.Patterns.Tests.Creational.Prototype
 			Console.WriteLine( "-----------------------DEEP--OBJECT-COUNTRY-----------" );
 
 			Console.WriteLine( "footballTeamRussia:" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
-			FootballTeam footballTeamRussiaShallow = this.footballTeamRussia.DeepClone();
+			FootballTeam footballTeamRussiaShallow = this._footballTeamRussia.DeepClone();
 			Console.WriteLine( "footballTeamRussiaShallow:" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
 
 			Console.WriteLine( "\n\nfootballTeamRussia.Captan.BornCountry = usa" );
-			this.footballTeamRussia.Captan.BornCountry = this.usa;
+			this._footballTeamRussia.Captan.BornCountry = this._usa;
 			Console.WriteLine( "footballTeamRussia" );
-			Console.WriteLine( this.footballTeamRussia.ToString() );
+			Console.WriteLine( this._footballTeamRussia.ToString() );
 
 			Console.WriteLine( "footballTeamRussiaShallow" );
 			Console.WriteLine( footballTeamRussiaShallow.ToString() );
